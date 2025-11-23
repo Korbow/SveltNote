e3d2bdcf0ddf07a9b467a71c4bdac41e62816eaf
Korbow <korbow.contact@gmail.com>
2025-11-23 13:54:47 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SveltNote/'
+  base: '/SveltNote/',
+  root: '.'   // facultatif, mais s’assure que la racine est correcte
 })
